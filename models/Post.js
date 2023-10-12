f55b9114dbf7a7ca18cdf0c7a51dca87be64fd6f
rrroy <hly5640@gmail.com>
2023-10-12 18:59:19 +1100

    create post

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,10 @@ const PostSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "user",
     },
+    title:{
+        type: String,
+        required: true
+    },
     text: {
         type: String,
         required: true,
--- routes/API/posts.js
+++ routes/API/posts.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { body, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts route"));
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      body("text", "Text is required").notEmpty(),
+      body("title", "Title is required").notEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return 400 status with errors array
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const avatar = user.avatar;
+      const name = user.name;
+
+      const newPost = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        avatar,
+        name,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 module.exports = router;
