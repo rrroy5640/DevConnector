6ae5c1a275c1c31baca54d17f18bc03d9ac8473f
rrroy <hly5640@gmail.com>
2024-02-16 14:08:44 +1100

    ready to deploy

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 // Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("API running"));
 
 // Define Routes
 app.use("/api/users", require("./routes/API/users"));
@@ -15,7 +15,17 @@ app.use("/api/auth", require("./routes/API/auth"));
 app.use("/api/profile", require("./routes/API/profile"));
 app.use("/api/posts", require("./routes/API/posts"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Serve index.html in build folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
